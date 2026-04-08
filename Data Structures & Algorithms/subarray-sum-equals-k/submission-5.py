09637ab83cf2ef3ed779285c1364841075f3695d
Jonas da Silva - UMD <90938278+ofjonvs@users.noreply.github.com>
2026-04-07 23:11:55 -0400

    Add: subarray-sum-equals-k - submission-5

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-5.py
@@ -0,0 +1,12 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        curSum = 0
+        hsh = defaultdict(lambda: 0)
+        hsh[0] = 1
+        res = 0
+        for n in nums:
+            curSum += n
+            res += hsh.get(curSum-k, 0)
+            hsh[curSum] += 1
+        return res
+
