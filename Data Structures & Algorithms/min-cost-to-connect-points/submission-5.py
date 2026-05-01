2ee8710af302a0cb3c58af3fdf76f5ce9fb72b29
Jonas da Silva - UMD <90938278+ofjonvs@users.noreply.github.com>
2026-05-01 13:10:21 -0400

    Add: min-cost-to-connect-points - submission-5

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-5.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        parents = [[None, 1] for _ in range(len(points))]
+        def find(point):
+            if parents[point][0] is not None:
+                point = find(parents[point][0])
+            return point
+
+        def union(point1, point2):
+            parent1, parent2 = find(point1), find(point2)
+            if parent1 == parent2:
+                return True
+            big, small = (parent1, parent2) if parents[parent1][1] > parents[parent2][1] else (parent2, parent1)
+            parents[small][0] = big
+            parents[big][1] += parents[small][1]
+            return False
+        
+        heap = []
+        for point1, (x1, y1) in enumerate(points):
+            for point2, (x2, y2) in enumerate(points[point1+1:], start=point1+1):           
+                heapq.heappush(heap, (abs(x1-x2)+abs(y1-y2), point1, point2))
+        
+        seenPoints = set()
+        totDist = 0
+        numEdges = 0
+        while numEdges != len(points)-1:
+            dist, point1, point2 = heapq.heappop(heap)
+            if union(point1, point2):
+                continue
+            seenPoints.add(point1), seenPoints.add(point2)
+            totDist += dist
+            numEdges += 1
+        return totDist
