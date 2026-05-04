963c1e094d146da07d64d1c3935de0d47e2f33d7
Jonas da Silva - UMD <90938278+ofjonvs@users.noreply.github.com>
2026-05-04 17:26:37 -0400

    Add: course-schedule-iv - submission-4

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-4.py
@@ -0,0 +1,17 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        adj = [[] for _ in range(numCourses)]
+        for c1, c2 in prerequisites:
+            adj[c1].append(c2)
+        visit = set()
+        prereqs = [set() for _ in range(numCourses)]
+        for course in range(numCourses):
+            def search(i):
+                if i in visit:
+                    return prereqs[i].union({i})
+                visit.add(i)
+                for course in adj[i]:
+                    prereqs[i].update(search(course))
+                return prereqs[i].union({i})
+            search(course)
+        return [c2 in prereqs[c1] for c1, c2 in queries]
