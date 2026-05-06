a74b9ede852732557476511c86c7325ed8056930
Jonas da Silva - UMD <90938278+ofjonvs@users.noreply.github.com>
2026-05-06 16:50:39 -0400

    Add: target-sum - submission-2

---
+++ Data Structures & Algorithms/target-sum/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        from functools import lru_cache
+
+        @lru_cache(len(nums)*sum(nums))
+        def recurse(i, curSum):
+            if i >= len(nums):
+                return int(curSum == target)
+            return recurse(i+1, curSum+nums[i]) + recurse(i+1, curSum-nums[i])
+        return recurse(0, 0)
+            
