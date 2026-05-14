87d726afc20a31b77a65a25bc41600d83d661700
Jonas da Silva - UMD <90938278+ofjonvs@users.noreply.github.com>
2026-05-13 22:45:33 -0400

    Add: count-subsequences - submission-7

---
+++ Data Structures & Algorithms/count-subsequences/submission-7.py
@@ -0,0 +1,12 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+
+        cache = [1]+[0]*(len(t))
+        for cs in s:
+            newCache = cache.copy()
+            for iT in range(1, len(t)+1):
+                if t[iT-1] == cs:
+                    newCache[iT] += cache[iT-1]
+            cache = newCache
+
+        return cache[-1]
