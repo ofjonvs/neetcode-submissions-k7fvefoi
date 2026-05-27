582baf1ae27e3f0deeb5e49379b919975506bd95
Jonas da Silva - UMD <90938278+ofjonvs@users.noreply.github.com>
2026-05-27 18:16:58 -0400

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.py
@@ -0,0 +1,35 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        cnt = Counter(t)
+        t = set(t)
+        l = r = 0
+        minLen = float('inf')
+        minL = minR = None
+        numValChars = 0
+        if s[0] in cnt:
+            cnt[s[0]] -= 1
+            if cnt[s[r]] == 0:
+                numValChars += 1
+        while l < len(s):
+            if numValChars != len(t):
+                r += 1
+                if r == len(s):
+                    break
+                if s[r] in cnt:
+                    cnt[s[r]] -= 1
+                    if cnt[s[r]] == 0:
+                        numValChars += 1
+            else: # valid substring
+                if r-l < minLen:
+                    minL, minR = l, r+1
+                    minLen = r-l
+                if s[l] in t:
+                    cnt[s[l]] += 1
+                    if cnt[s[l]] > 0:
+                        numValChars -= 1
+                l += 1
+        return s[minL:minR] if minL is not None else ''
+                
+                    
+                
+            
