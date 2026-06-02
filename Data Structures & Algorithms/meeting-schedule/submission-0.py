aa165c110de189728e64ea5d6d81d1c99fd06d08
Jonas da Silva - UMD <90938278+ofjonvs@users.noreply.github.com>
2026-06-01 21:41:59 -0400

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.py
@@ -0,0 +1,17 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        from operator import attrgetter
+        intervals.sort(key=attrgetter('start'))
+
+        for i in range(1, len(intervals)):
+            if intervals[i].start < intervals[i-1].end:
+                return False
+        return True
