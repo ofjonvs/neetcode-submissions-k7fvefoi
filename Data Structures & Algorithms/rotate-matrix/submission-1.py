d8012911274c32f66582d03d081303519ca1eac1
Jonas da Silva - UMD <90938278+ofjonvs@users.noreply.github.com>
2026-06-08 18:20:20 -0400

    Add: rotate-matrix - submission-1

---
+++ Data Structures & Algorithms/rotate-matrix/submission-1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        for layer in range(len(matrix) // 2):
+            for col in range(layer, len(matrix)-layer-1):
+                tmp = matrix[layer][col]
+                for r2, c2 in ((col, -layer-1), (-layer-1, -col-1), (-col-1, layer), (layer, col)):
+                    matrix[r2][c2], tmp =  tmp, matrix[r2][c2]
