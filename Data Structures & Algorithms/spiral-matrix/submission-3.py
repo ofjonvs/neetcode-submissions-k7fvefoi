f111320a9e2a05e0b64d9a3119e78a217c451ad3
Jonas da Silva - UMD <90938278+ofjonvs@users.noreply.github.com>
2026-06-08 18:48:06 -0400

    Add: spiral-matrix - submission-3

---
+++ Data Structures & Algorithms/spiral-matrix/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        res = []
+        for layer in range((min(len(matrix), len(matrix[0]))+1)//2):
+            colStart, colEnd = layer, len(matrix[0])-layer-1
+            
+            for c in range(colStart, colEnd):
+                res.append(matrix[layer][c])
+
+            for r in range(layer, len(matrix)-layer-1):
+                res.append(matrix[r][colEnd])
+
+            for c in range(colEnd, colStart, -1):
+                res.append(matrix[-layer-1][c])
+
+            for r in range(len(matrix)-layer-1, layer, -1):
+                print(colStart, r)
+                res.append(matrix[r][colStart])
+
+        if len(res) != len(matrix)*len(matrix[0]):
+            res.append(matrix[len(matrix)//2][len(matrix[0])//2])
+        return res[:len(matrix)*len(matrix[0])]
+            
