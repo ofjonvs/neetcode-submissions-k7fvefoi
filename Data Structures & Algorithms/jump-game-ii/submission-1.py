95b5c0558dcb5a57b363d446d7a20cd79df09ebf
Jonas da Silva - UMD <90938278+ofjonvs@users.noreply.github.com>
2026-06-20 14:16:52 -0400

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        import functools
+
+        @functools.cache
+        def recurse(i):
+            if i >= len(nums)-1:
+                return 0
+            if nums[i] == 0:
+                return 100
+            return 1 + min(recurse(j) for j in range(i+nums[i], i, -1))
+
+        return recurse(0)
