593f36cbd3d564fc607b63fd43854febfe6dfe4f
Jonas da Silva - UMD <90938278+ofjonvs@users.noreply.github.com>
2026-07-11 13:00:06 -0400

    Add: generate-parentheses - submission-2

---
+++ Data Structures & Algorithms/generate-parentheses/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        s = ''
+        def recurse(L, R):
+            nonlocal s
+            sTemp = s
+            if not L:
+                res.append(s+')'*R)
+            elif L == R:
+                s += '('
+                recurse(L-1, R)
+                # s = sTemp
+            else:
+                s += '('
+                recurse(L-1, R)
+                s = sTemp
+                s += ')'
+                recurse(L, R-1)
+                # s = sTemp
+        recurse(n, n)   
+        return res    
