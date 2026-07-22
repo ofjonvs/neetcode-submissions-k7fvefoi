2d92602d4245be30590522832761de7b999337be
Jonas da Silva - UMD <90938278+ofjonvs@users.noreply.github.com>
2026-07-22 09:15:57 -0400

    Add: combination-target-sum-ii - submission-5

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-5.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        out, path, curSum = [], [], 0
+        def recurse(i):
+            nonlocal curSum
+            if curSum == target:
+                out.append(path.copy())
+                return
+            if i >= len(candidates) or curSum > target:
+                return
+            
+            path.append(candidates[i])
+            curSum += candidates[i]
+            recurse(i+1)
+            path.pop()
+            curSum -= candidates[i]
+            
+            while i + 1 < len(candidates) and candidates[i] == candidates[i+1]:
+                i += 1
+            recurse(i+1)
+
+        recurse(0)
+        return out
