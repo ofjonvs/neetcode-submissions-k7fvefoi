da477521052c291ccd58c87143b6230d723a4fef
Jonas da Silva - UMD <90938278+ofjonvs@users.noreply.github.com>
2026-07-29 19:10:05 -0400

    Add: word-break-ii - submission-1

---
+++ Data Structures & Algorithms/word-break-ii/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        wordDict = set(wordDict)
+        out = []
+        def recurse(i, curWord, curSentence):
+            if i == len(s)-1:
+                curWord += s[i]
+                if curWord in wordDict:
+                    out.append((curSentence + ' ' + curWord)[1:])
+                return
+                
+            curWord += s[i]
+            if curWord in wordDict:
+                recurse(i+1, '', curSentence+' '+curWord)
+            recurse(i+1, curWord, curSentence)
+        recurse(0, '', '')
+        print(out)
+        return out
